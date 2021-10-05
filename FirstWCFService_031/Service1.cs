4b99a94326138bba74cb04218bf14b0e2fd0b6dc
Bimahayunugraha <bimahayunugraha@gmail.com>
2021-10-05 20:16:05 +0700

    Add project files.

---
+++ FirstWCFService_031/Service1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FirstWCFService_031
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public double AddNumber(double number1, double number2)
+        {
+            return (number1 + number2);
+        }
+        public double SubNumber(double number1, double number2)
+        {
+            return (number1 - number2);
+        }
+        public double MultipleNumber(double number1, double number2)
+        {
+            return (number1 * number2);
+        }
+        public double DivideNumber(double number1, double number2)
+        {
+            return (number1 / number2);
+        }
+    }
+}
